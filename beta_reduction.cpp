eca3b307964086d07bea00a073f11bdbd5af35ed
gasin <gasinsato@gmail.com>
2017-07-15 03:24:26 +0900

    implement beta-reduction

--- beta_reduction.cpp
+++ beta_reduction.cpp
@@ -60,7 +60,7 @@ vector<int> de_bruijn(vector<string> vec) {
     
     const int undefined = -100;
     vector<int> ret((int)vec.size(), undefined);
-    // lambda..-1, right_bracket..-2, left_bracket..-3
+    // lambda..-1, left_bracket..-2, right_bracket..-3
     
     for(int i = 0; i < (int)vec.size(); i++) {
         if(ret[i] != undefined) {
@@ -121,7 +121,7 @@ vector<int> de_bruijn(vector<string> vec) {
     
     {
         int cnt = 0;
-        stack<int> blacket_type;
+        stack<bool> blacket_type;
         for(int i = 0; i < (int)vec.size(); i++) {
             if(vec[i] == "(") {
                 if(vec[i+1][0] == '\\') {
@@ -145,21 +145,120 @@ vector<int> de_bruijn(vector<string> vec) {
     return ret;
 }
 
+vector<int> reduct(vector<int> pre) {
+    vector<int> nxt;
+    while(true) {
+        bool end = true;
+        nxt.clear();
+        for(int i = 0; i < (int)pre.size(); i++) {
+            if(pre[i] != -1) continue;
+            int cnt = 0;
+            int en = (int)pre.size()-1;
+            for(int j = i+1; j < (int)pre.size(); j++) {
+                if(pre[j] == -2) {
+                    cnt++;
+                } else if(pre[j] == -3){
+                    cnt--;
+                    if(cnt == -1) {
+                        en = j;
+                        break;
+                    }
+                }
+            }
+            if(en == (int)pre.size()-1) continue;
+            
+            vector<int> item;
+            if(pre[en+1] >= 0) {
+                item.push_back(pre[en+1]);
+            } else if(pre[en+1] == -2) {
+                cnt = 0;
+                item.push_back(pre[en+1]);
+                for(int j = en+2; j < (int)pre.size(); j++) {
+                    if(pre[j] == -2) {
+                        cnt++;
+                    } else if(pre[j] == -3) {
+                        cnt--;
+                    }
+                    item.push_back(pre[j]);
+                    if(cnt == -1) break;
+                }
+            }
+            if(item.size() == 0) continue;
+            
+            for(int j = 0; j < i-1; j++) {
+                nxt.push_back(pre[j]);
+            }
+            
+            stack<bool> bracket_type;
+            cnt = 0;
+            
+            for(int j = i+1; j < en; j++) {
+                if(pre[j] == -2) {
+                    if(pre[j+1] == -1) {
+                        bracket_type.push(true);
+                        cnt++;
+                    } else {
+                        bracket_type.push(false);
+                    }
+                }
+                if(pre[j] == -3) {
+                    if(bracket_type.top() == true) {
+                        cnt--;
+                    }
+                    bracket_type.pop();
+                }
+                if(pre[j] < 0) {
+                    nxt.push_back(pre[j]);
+                } else if(pre[j] < cnt) {
+                    nxt.push_back(pre[j]);
+                } else if(pre[j] > cnt) {
+                    nxt.push_back(pre[j]-1);
+                } else {
+                    for(int k = 0; k < (int)item.size(); k++) {
+                        if(item[k] >= 0) {
+                            nxt.push_back(item[k]+cnt);
+                        } else {
+                            nxt.push_back(item[k]);
+                        }
+                    }
+                }
+            }
+            for(int j = en+item.size()+1; j < (int)pre.size(); j++) {
+                nxt.push_back(pre[j]);
+            }
+            end = false;
+            break;
+        }
+        if(end) break;
+        pre = nxt;
+    }
+    return pre;
+}
+
 string beta_reduction(string input) {
     input = add_bracket(input);
     cout << "add_bracket : " << input << endl;
     
     vector<string> formula = str2vec(input);
-    cout << "str2vec:";
+    cout << "str2vec : ";
     for(int i = 0; i < (int)formula.size(); i++) {
         cout << "\"" << formula[i] << "\" ";
     }
     cout << endl;
     
     vector<int> bruijn_formula = de_bruijn(formula);
-    cout << "bruijn_formula:";
+    cout << "bruijn_formula : ";
     for(int i = 0; i < (int)bruijn_formula.size(); i++) {
         cout << bruijn_formula[i] << " ";
     }
+    cout << endl;
+    
+    vector<int> reducted_formula = reduct(bruijn_formula);
+    cout << "reducted_formula : ";
+    for(int i = 0; i < (int)reducted_formula.size(); i++) {
+        cout << reducted_formula[i] << " ";
+    }
+    cout << endl;
+    
     return "";
 }
